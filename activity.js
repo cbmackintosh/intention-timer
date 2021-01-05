1bb152d9da907452f30ce296b1bdbee05ac01336
Nikki <piknik@me.com>
2021-01-05 13:41:46 -0700

    Added Activity class structure

---
+++ activity.js
@@ -0,0 +1,25 @@
+class Activity {
+  constructor(category, description, minutes, seconds, completed, id) {
+    this.category = category
+    this.description = description
+    this.minutes = minutes
+    this.seconds = seconds
+    this.completed = completed
+    this.id = id
+  }
+
+  countdown(minutes, seconds) {
+    // get minutes and seconds from form
+    // use those in setInterval
+  }
+
+  markComplete() {
+
+  }
+
+  saveToStorage() {
+
+  }
+}
+
+module.exports = Activity;
